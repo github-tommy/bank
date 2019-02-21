6bfde52a50b42a6e64fd66027198e0b5b107b2c7
tom <tommy.surjadi@gmail.com>
2019-02-20 16:40:34 -0800

    Added available balance in the alert, in addition to balance. Added comment in js

--- bank.js
+++ bank.js
@@ -1,17 +1,23 @@
-//banking assignment - to loop 4 functions of withdraw deposit view quit /WDVQ loop
-// ====================================================================================
-// declare a balance and while loop? ==================================
+//banking assignment - Do a while WDVQ loop
+// Alert user balance and available balance to wdraw - net of 300$ min required to keep ==================================
 
 let quit = false;
 let bal = 1000;
-alert('Your available balance is US$'+ bal);
+let balAvailable = bal - 300;
+
+alert('Your available balance is US$'+ bal + '. You may withdraw up to US$'+ balAvailable);
+
+//user can quit anytime if Q is selected.
 
 while(quit === false) {
     let input = prompt ("Enter your command: 'W' for withdrawal, 'D' for deposit, 'V' for view' or 'Q' for quit in Capital Letter");
-  
+
       if (input === 'Q') {
     quit = true;
 } 
+
+// user can withdraw up balance - 300 min to keep in account
+
 else if (input === 'W') {
     var withdrawal;
     var withdrawal = prompt('enter the amount you would like to withdraw');
@@ -26,6 +32,8 @@ else if (input === 'W') {
     } else (alert('Insufficient fund. You may try again for a smaller amount. A minimum balance of US$300 is required by Norton Bank at all times.'));
 }
 
+//user can deposit up t0 50k per transaction
+
 else if(input === 'D') {
     var deposit;
     var deposit = prompt('enter the amount you would like to deposit');
@@ -39,6 +47,8 @@ else if(input === 'D') {
     
     } else alert('Your deposit is not accepted. Please try again for a smaller amount. A deposit is subject to a maximum of US$50k per transaction.');
 }
+
+//to alert user low balance 1k - to top up
 
 else if(input ==='V') {
     var view;
